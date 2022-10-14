cd6cc0643d957664f84dd4140cd2d37b62c574eb
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-10-14 21:49:29 +0530

    add. test for gildedrose aged brie expiry sellin

--- src/test/java/com/a/introduction/gildedrose/GildedRoseSolutionTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseSolutionTest.java
@@ -8,30 +8,54 @@ public class GildedRoseSolutionTest {
 
     private int NOT_EXPIRED_SELL_IN = 15;
     private int EXPIRED_SELL_IN = -1;
-    private int QUALITY = 3;
+    private int DEFAULT_QUALITY = 3;
     private final String DEFAULT_ITEM = "DEFAULT_ITEM";
+    private final String AGED_BRIE = "Aged Brie";
 
     @Test
     public void shouldDecreaseQualityByOneForNonExpiredItem(){
-        GildedRose app = createGildedRoseWithDefaultItem(DEFAULT_ITEM, NOT_EXPIRED_SELL_IN,QUALITY);
+        GildedRose app = createGildedRoseWithDefaultItem(DEFAULT_ITEM, NOT_EXPIRED_SELL_IN,DEFAULT_QUALITY);
 
         app.updateQuality();
 
-        Item expected = new Item(DEFAULT_ITEM, NOT_EXPIRED_SELL_IN - 1, QUALITY - 1);
+        Item expected = new Item(DEFAULT_ITEM, NOT_EXPIRED_SELL_IN - 1, DEFAULT_QUALITY - 1);
 
         assertItem(expected, app.items[0]);
     }
 
     @Test
     public void shouldDecreaseQualityByTwoForExpiredItem(){
-        GildedRose app = createGildedRoseWithDefaultItem(DEFAULT_ITEM, EXPIRED_SELL_IN, QUALITY);
+        GildedRose app = createGildedRoseWithDefaultItem(DEFAULT_ITEM, EXPIRED_SELL_IN, DEFAULT_QUALITY);
 
         app.updateQuality();
 
-        Item expected = new Item(DEFAULT_ITEM, EXPIRED_SELL_IN - 1, QUALITY - 2);
+        Item expected = new Item(DEFAULT_ITEM, EXPIRED_SELL_IN - 1, DEFAULT_QUALITY - 2);
 
         assertItem(expected, app.items[0]);
     }
+
+    @Test
+    public void shouldIncreaseQualityByOneForAgedBrie(){
+        GildedRose app = createGildedRoseWithDefaultItem(AGED_BRIE, NOT_EXPIRED_SELL_IN, DEFAULT_QUALITY);
+
+        app.updateQuality();
+
+        Item expected = new Item(AGED_BRIE, NOT_EXPIRED_SELL_IN - 1, DEFAULT_QUALITY + 1);
+
+        assertItem(expected, app.items[0]);
+    }
+
+    @Test
+    public void shouldIncreaseQualityByTwoForAgedBrie(){
+        GildedRose app = createGildedRoseWithDefaultItem(AGED_BRIE, EXPIRED_SELL_IN, DEFAULT_QUALITY);
+
+        app.updateQuality();
+
+        Item expected = new Item(AGED_BRIE, EXPIRED_SELL_IN - 1, DEFAULT_QUALITY + 2);
+
+        assertItem(expected, app.items[0]);
+    }
+
 
     private GildedRose createGildedRoseWithDefaultItem(String itemName, int sell_in, int quality){
         Item item = new Item(itemName, sell_in,quality);

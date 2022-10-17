fdc76e01942f213bcc37d96094bb39fd5585639f
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-10-17 20:26:53 +0530

    refactor. customer bo test

---
+++ src/test/java/com/b/simple/design/business/customer/CustomerBOTestSolution.java
@@ -0,0 +1,45 @@
+package com.b.simple.design.business.customer;
+
+import com.b.simple.design.business.exception.DifferentCurrenciesException;
+import com.b.simple.design.model.customer.*;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerBOTestSolution {
+
+    private CustomerBO customerBO = new CustomerBOImpl();
+
+    @Test
+    public void testCustomerProductSum_TwoProductsSameCurrencies() throws DifferentCurrenciesException {
+
+        Amount[] amounts = {new AmountImpl(new BigDecimal("5.0"), Currency.EURO), new AmountImpl(new BigDecimal("6.0"), Currency.EURO)};
+
+        List<Product> products = createProductWithAmount(amounts);
+
+        Amount expected = new AmountImpl(new BigDecimal("11.0"), Currency.EURO);
+        Amount actual = customerBO.getCustomerProductsSum(products);
+
+        assertAmount(expected, actual);
+    }
+
+    private void assertAmount(Amount expected, Amount actual) {
+        assertEquals(expected.getCurrency(), actual.getCurrency());
+        assertEquals(expected.getValue(), actual.getValue());
+    }
+
+    private List<Product> createProductWithAmount(Amount[] amounts) {
+        return Arrays.stream(amounts)
+                .map(amount ->
+                        new ProductImpl(100, "Product 15", ProductType.BANK_GUARANTEE,
+                                amount))
+                .collect(Collectors.toList());
+    }
+
+}

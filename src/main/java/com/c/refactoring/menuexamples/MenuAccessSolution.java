cc6cc1cbc64147c8eb1ec600645c7715c837d8af
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-10-18 20:49:43 +0530

    refactor. menuAccess set authorization method

---
+++ src/main/java/com/c/refactoring/menuexamples/MenuAccessSolution.java
@@ -0,0 +1,31 @@
+package com.c.refactoring.menuexamples;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MenuAccessSolution {
+    public void setAuthorizationsInEachMenus(List<MenuItem> menuItemsList, Role[] roles) {
+        if(roles == null){
+            return;
+        }
+        menuItemsList.stream()
+                .forEach(menuItem -> setAccessForMenuItems(menuItem, roles));
+
+    }
+
+    private void setAccessForMenuItems(MenuItem menuItem, Role[] roles) {
+        if(doUserHasAnyRole(roles,menuItem.getReadAccessRole())){
+            menuItem.setAccess(Constants.READ);
+            menuItem.setVisible(true);
+        }
+        else if(doUserHasAnyRole(roles, menuItem.getWriteAccessRole())){
+            menuItem.setAccess(Constants.WRITE);
+            menuItem.setVisible(true);
+        }
+    }
+
+    private boolean doUserHasAnyRole(Role[] roles, String userAccessRole) {
+        return Arrays.stream(roles)
+                .anyMatch(role -> role.getName().equals(userAccessRole));
+    }
+}
